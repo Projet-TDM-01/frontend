f09c3ae11f1a67f74eb539297af86f9a01dd9abb
MerakchiHibaa <ninamimi305@gmail.com>
2022-06-24 11:36:31 +0100

    datepicker has a problem with the format

--- projet_tdm/app/src/main/java/com/example/projet_tdm/reservationActivity.kt
+++ projet_tdm/app/src/main/java/com/example/projet_tdm/reservationActivity.kt
@@ -1,10 +1,13 @@
 package com.example.projet_tdm
 
+import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
 import java.util.*
 
 class reservationActivity : AppCompatActivity() {
@@ -13,6 +16,10 @@ class reservationActivity : AppCompatActivity() {
 
     private lateinit var tvTimeEnd : TextView
     private lateinit var btnTimePickerEnd : Button
+
+    private  lateinit var btnDatePicker : Button
+    private lateinit var  tvDate : TextView
+    var formatDate = SimpleDateFormat("dd-MMMM-yyyy" , Locale.UK)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,5 +48,30 @@ class reservationActivity : AppCompatActivity() {
             } , startHourEnd , startMinuteEnd ,false).show()
 
         }
+
+
+        btnDatePicker = findViewById(R.id.btnDatePicker)
+        btnDatePicker.setOnClickListener{
+            val getDate = Calendar.getInstance()
+            val datePicker = DatePickerDialog(this , android.R.style.Theme_Holo_Light_Dialog_MinWidth , DatePickerDialog.OnDateSetListener{ datePicker, i, i2, i3 ->
+                val selectDate = Calendar.getInstance()
+                selectDate.set(Calendar.YEAR,i)
+                selectDate.set(Calendar.MONTH , i2)
+                selectDate.set(Calendar.DAY_OF_MONTH , i3)
+                val date = formatDate.format(selectDate.time)
+                Toast.makeText(this , "Date : " +date , Toast.LENGTH_SHORT).show()
+                tvDate.text = date
+
+            } , getDate.get(Calendar.YEAR) , getDate.get(Calendar.MONTH) , getDate.get(Calendar.DAY_OF_MONTH))
+
+            datePicker.show()
+
+        }
+
     }
+    /*private fun updateLable(myCalendar : Calendar) {
+        val myFormat = "dd-MM-yyyy"
+        val sdf = sdf =
+
+    }*/
 }
