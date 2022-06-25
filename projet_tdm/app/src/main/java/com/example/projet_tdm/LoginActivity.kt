ff8168b2bdcba9dae77ea9ef019ff1c99d6b0268
MerakchiHibaa <ninamimi305@gmail.com>
2022-06-26 00:48:07 +0100

    dont pull just yet, login is yet to be modified

--- projet_tdm/app/src/main/java/com/example/projet_tdm/LoginActivity.kt
+++ projet_tdm/app/src/main/java/com/example/projet_tdm/LoginActivity.kt
@@ -1,5 +1,16 @@
 package com.example.projet_tdm
 
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.projet_tdm.ViewModels.UserViewModel
+import com.example.projet_tdm.databinding.ActivityLoginBinding
+import com.example.projet_tdm.retrofit.authObject
+import com.example.projet_tdm.retrofit.authentification
+import com.facebook.CallbackManager
+import com.facebook.login.widget.LoginButton
+import io.reactivex.disposables.CompositeDisposable
+
 
 /*
 class LoginActivity : AppCompatActivity() {
@@ -157,21 +168,23 @@ class LoginActivity : AppCompatActivity() {
 }
 
 */
-/*
-class LoginActivity : AppCompatActivity() {
-    lateinit var  fbBtn : LoginButton;
-    lateinit var loginbtn : Button
-    var TAG = "Parks"
-    lateinit var  mainBinding : ActivityLoginBinding
-    lateinit var callbackManager : CallbackManager
-lateinit var userViewModel : UserViewModel
 
-    lateinit var authService : authentification
+class LoginActivity : AppCompatActivity() {
+    lateinit var fbBtn: LoginButton;
+    lateinit var loginbtn: Button
+    var TAG = "Parks"
+    lateinit var mainBinding: ActivityLoginBinding
+    lateinit var callbackManager: CallbackManager
+    lateinit var userViewModel: UserViewModel
+
+    lateinit var authService: authentification
     internal var compositeDiposable = CompositeDisposable()
 
     override fun onStop() {
         compositeDiposable.clear()
         super.onStop()
+
+        
     }
 
 
@@ -181,146 +194,6 @@ lateinit var userViewModel : UserViewModel
         authService = retrofit.create(authService::class.java)
         loginbtn = findViewById(R.id.login)
 
-        ViewModelProvider(requireActivity()).get(UserViewModel::class.java)
-        userViewModel.errorMessage.value = null
-        setupListers()
-        ActivityLoginBinding..setOnClickListener {
-            activity?.findNavController(R.id.navHost)
-                ?.navigate(R.id.action_loginFragment_to_registerFragment)
-        }
-        Binding.login.setOnClickListener {
-            if (isValid()) {
-                val input = Binding.email.text.toString()
-                val passwordUser = Binding.mdp.text.toString()
-                var data: HashMap<String, String> = HashMap<String, String>()
-                if (android.util.Patterns.EMAIL_ADDRESS.matcher(input).matches()) {
-                    data["email"] = input;
-                    data["mot_de_passe"] = passwordUser;
-                    utilisateurViewModel.connexionUtilisateurEmail(data)
-                } else if (android.util.Patterns.PHONE.matcher(input).matches()) {
-                    data["numero_telephone"] = input;
-                    data["mot_de_passe"] = passwordUser;
-                    utilisateurViewModel.connexionUtilisateurNumeroTelephone(data)
-                } else {
-                    Toast.makeText(
-                        requireActivity(),
-                        "Les données saisies sont invalides!",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-        }
-        utilisateurViewModel.loading.observe(viewLifecycleOwner, Observer { loading ->
-            if (!loading) {
-                Binding.progressBarLogin.visibility = View.GONE
-            } else {
-                Binding.progressBarLogin.visibility = View.VISIBLE
-            }
-        })
-        utilisateurViewModel.errorMessage.observe(viewLifecycleOwner, Observer { message ->
-            if (message != null) {
-                Toast.makeText(
-                    requireActivity(),
-                    "Une erreur s'est produite",
-                    Toast.LENGTH_SHORT
-                ).show()
-                Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
-            }
-        })
-        utilisateurViewModel.utilisateurs.observe(viewLifecycleOwner, Observer { utilisateurs ->
-            if (utilisateurViewModel.loading.value == false) {
-                if (utilisateurs != null && utilisateurs.isEmpty()) {
-                    var text = "La connexion a échouée"
-                    val duration = Toast.LENGTH_LONG
-                    var toast = Toast.makeText(context, text, duration)
-                    toast.show()
-                    text = "Email ou mot de passe incorrect"
-                    toast = Toast.makeText(context, text, duration)
-                    toast.show()
-                } else if (utilisateurs != null && utilisateurs.isNotEmpty()) {
-                    val itemMenu: MenuItem = utilisateurViewModel.menu!!.findItem(R.id.btnLogout)
-                    itemMenu.isVisible = true
-                    var utilisateur = utilisateurViewModel.utilisateurs.value?.get(0)
-                    val pr = requireActivity().getSharedPreferences("db_privee", Context.MODE_PRIVATE)?.edit()
-                    pr?.putBoolean("connected", true)
-                    pr?.putString("email", utilisateur?.email)
-                    pr?.putString("id_utilisateur", utilisateur?.id_utilisateur.toString())
-                    pr?.putString("numero_telephone", utilisateur?.numero_telephone)
-                    pr?.putString("mot_de_passe", utilisateur?.mot_de_passe)
-                    pr?.putString("nom", utilisateur?.nom)
-                    pr?.putString("prenom", utilisateur?.prenom)
-                    pr?.apply()
-                    activity?.findNavController(R.id.navHost)
-                        ?.navigate(R.id.action_loginFragment_to_mesReservationFragment2)
-                }
-            }
-        })
-        utilisateurViewModel.errorMessage.observe(requireActivity(), Observer { message ->
-            if (message != null) {
-                Toast.makeText(requireContext(), "Une erreur s'est produite", Toast.LENGTH_SHORT)
-                    .show()
-                Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
-            }
-        })
+
     }
-
-    inner class textFieldValidation(private val view: View) : TextWatcher {
-        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-        }
-
-        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            when (view.id) {
-                R.id.email -> {
-                    validateInput()
-                }
-                R.id.mdp -> {
-                    validatePassword()
-                }
-            }
-        }
-
-        override fun afterTextChanged(p0: Editable?) {
-        }
-    }
-
-    fun isValid(): Boolean = validateInput() && validatePassword()
-
-    fun validateInput(): Boolean {
-        if (Binding.email.text.toString().trim().isEmpty()) {
-            Binding.emailTextInputLayout.error = "Champs requis!"
-            Binding.email.requestFocus()
-            return false
-        } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(Binding.email.text.toString())
-                .matches() && !android.util.Patterns.PHONE.matcher(Binding.email.text.toString())
-                .matches()
-        ) {
-            Binding.emailTextInputLayout.error = "Email/Numero de telephone invalide!"
-            Binding.email.requestFocus()
-            return false
-        } else {
-            Binding.emailTextInputLayout.isErrorEnabled = false
-        }
-        return true
-    }
-
-    fun validatePassword(): Boolean {
-        if (Binding.mdp.text.toString().isEmpty()) {
-            Binding.passwordTextInputLayout.error = "Champs requis!"
-            Binding.mdp.requestFocus()
-            return false
-        } else if (Binding.mdp.text.toString().length <= 6) {
-            Binding.passwordTextInputLayout.error =
-                "Mot de passe doit contenir au moins 6 caracteres!"
-            Binding.mdp.requestFocus()
-            return false
-        } else {
-            Binding.passwordTextInputLayout.isErrorEnabled = false
-        }
-        return true
-    }
-
-    fun setupListers() {
-        Binding.email.addTextChangedListener(textFieldValidation(Binding.email))
-        Binding.mdp.addTextChangedListener(textFieldValidation(Binding.mdp))
-    }
-}*/
+}
